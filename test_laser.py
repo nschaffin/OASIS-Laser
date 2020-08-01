76381d2715a72a4ddb92020bf6e55ab57976612c
ExpandingDev <tylersengia@gmail.com>
2020-07-31 22:43:08 -0400

    started unittest framework

---
+++ test_laser.py
@@ -0,0 +1,9 @@
+import unittest
+from laser_control import Laser
+
+class TestLaserCommands(unittest.TestCase):
+    #TODO: Implement unittests
+    
+    
+if __name__ == "__main__":
+    unittest.main()
