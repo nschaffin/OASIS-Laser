3ac556a2560025858cadb9f8f7afe8c78015b4f3
ExpandingDev <tylersengia@gmail.com>
2020-10-02 06:28:14 +0000

    Fixing return values because actual laser does not return capital letters

--- ujlaser/test/test_laser.py
+++ ujlaser/test/test_laser.py
@@ -33,7 +33,7 @@ class TestLaserCommands(unittest.TestCase):
         """Tests Laser._send_command, feeds in a mock serial object. Checking to make sure that write is called and that it returns the reponse we give it."""
         serial_mock = Mock()
 
-        serial_mock.read_until = Mock(return_value=b"OK\r")
+        serial_mock.read_until = Mock(return_value=b"ok\r\n")
         serial_mock.write = Mock()
 
         l = Laser()
@@ -42,13 +42,13 @@ class TestLaserCommands(unittest.TestCase):
         
         l._ser = serial_mock
         l.connected = True
-        assert l._send_command("HELLO WORLD") == b"OK\r" # Ensure that we are returning the serial response
+        assert l._send_command("HELLO WORLD") == b"ok\r\n" # Ensure that we are returning the serial response
         serial_mock.write.assert_called_once_with(";LA:HELLO WORLD\r".encode("ascii")) # Ensure that the correct command format is being used
 
     def test_arm_command(self):
         """Tests Laser.arm(), should return True because we are feeding it a nominal response, and this should result in serial.write being called with the correct command"""
         serial_mock = Mock()
-        serial_mock.read_until = Mock(return_value=b"OK\r")
+        serial_mock.read_until = Mock(return_value=b"ok\r\n")
         serial_mock.write = Mock() # NOTE: Major difference from pyserial class, does not return the number of bytes written.
 
         l = Laser()
@@ -66,7 +66,7 @@ class TestLaserCommands(unittest.TestCase):
     def test_disarm_command(self):
         """Tests Laser.disarm(), should return True because we are feeding it a nominal response, and this should result in serial.write being called with the correct command"""
         serial_mock = Mock()
-        serial_mock.read_until = Mock(return_value=b"OK\r")
+        serial_mock.read_until = Mock(return_value=b"ok\r\n")
         serial_mock.write = Mock() # NOTE: Major difference from pyserial class, does not return the number of bytes written.
 
         l = Laser()
@@ -130,7 +130,7 @@ class TestLaserCommands(unittest.TestCase):
         """Tests Laser.set_diode_trigger, feeds in a mock serial object. Makes sure that the correct data is written and that the properties of the class are changed."""
         serial_mock = Mock()
 
-        serial_mock.read_until = Mock(return_value=b"OK\r")
+        serial_mock.read_until = Mock(return_value=b"ok\r\n")
         serial_mock.write = Mock()
 
         l = Laser()
@@ -147,7 +147,7 @@ class TestLaserCommands(unittest.TestCase):
         serial_mock.write.assert_called_once_with(";LA:DT 1\r".encode("ascii"))
         assert l.diodeTrigger == 1
 
-        serial_mock.read_until = Mock(return_value=b"?1") # Make sure we return False is the laser returns an error
+        serial_mock.read_until = Mock(return_value=b"?1\r\n") # Make sure we return False is the laser returns an error
         serial_mock.write = Mock()
 
         with self.assertRaises(LaserCommandError):
@@ -237,7 +237,7 @@ class TestLaserCommands(unittest.TestCase):
         """Tests Laser.set_pulse_width, feeds in a mock serial object. Makes sure that the correct data is written and that the properties of the class are changed."""
         serial_mock = Mock()
 
-        serial_mock.read_until = Mock(return_value=b"OK\r")
+        serial_mock.read_until = Mock(return_value=b"ok\r\n")
         serial_mock.write = Mock()
 
         l = Laser()
@@ -270,7 +270,7 @@ class TestLaserCommands(unittest.TestCase):
         """Tests Laser.set_energy_mode, feeds in a mock serial object. Makes sure that the correct data is written and that the properties of the class are changed."""
         serial_mock = Mock()
 
-        serial_mock.read_until = Mock(return_value=b"OK\r")
+        serial_mock.read_until = Mock(return_value=b"ok\r\n")
         serial_mock.write = Mock()
 
         l = Laser()
@@ -287,7 +287,7 @@ class TestLaserCommands(unittest.TestCase):
         serial_mock.write.assert_called_once_with(";LA:EM 1\r".encode("ascii"))
         assert l.energyMode == 1
 
-        serial_mock.read_until = Mock(return_value=b"?1") # Make sure we return False is the laser returns an error
+        serial_mock.read_until = Mock(return_value=b"?1\r\n") # Make sure we return False is the laser returns an error
         serial_mock.write = Mock()
 
         with self.assertRaises(LaserCommandError):
@@ -301,7 +301,7 @@ class TestLaserCommands(unittest.TestCase):
         """Tests Laser.set_diode_current, feeds in a mock serial object. Makes sure that the correct data is written and that the properties of the class are changed."""
         serial_mock = Mock()
 
-        serial_mock.read_until = Mock(return_value=b"OK\r")
+        serial_mock.read_until = Mock(return_value=b"ok\r\n")
         serial_mock.write = Mock()
 
         l = Laser()
@@ -325,7 +325,7 @@ class TestLaserCommands(unittest.TestCase):
         assert l.diodeCurrent == 100
         assert l.energyMode == 0
 
-        serial_mock.read_until = Mock(return_value=b"?1") # Emulate the laser returning a failure code.
+        serial_mock.read_until = Mock(return_value=b"?1\r\n") # Emulate the laser returning a failure code.
         serial_mock.write = Mock()
 
         # Reset the energy mode to test that it DOES NOT gets switched to manual (0)
